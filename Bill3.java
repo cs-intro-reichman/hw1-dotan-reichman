8d6b01d865c2c3341a31a2cb7ca9a5935104a86e
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-04 12:29:52 +0200

    Final test?

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		int money=Integer.parseInt(args[3]);	    // Replace this comment with the rest of your code   
-	System.out.println(name3+" "+ name2 + " "+ name1+ " pay:"+ Math.ceil(money/3));
+	System.out.println(name3+", "+ name2 + " and "+ name1+ " pay:"+ Math.ceil((money)/3+1));
 	}
 }
